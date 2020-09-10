55d5756a89af9badf16a4ab6a98e9b6a978dedd3
pinsara <aasithp@gmail.com>
2020-09-10 13:24:35 +0530

    Init Commit

--- server.js
+++ server.js
@@ -30,12 +30,16 @@ http.createServer(function (req, res) {
   // Generate a report using the sample template provided by carbone module
   // This LibreOffice template contains "Hello {d.firstname} {d.lastname} !"
   // Of course, you can create your own templates!
-  carbone.render('./node_modules/carbone/examples/test.docx', data, function(err, result){
+  carbone.render('./test.docx', data, function(err, result){
     if (err) {
       return console.log(err);
     }
     // write the result
     fs.writeFileSync('name.docx', result);
+	
+	res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+    res.write(result);
+    return res.end();
     
   });
 
