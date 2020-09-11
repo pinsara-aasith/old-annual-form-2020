12cb47041838f1756a3c8ca177e02231bd8a888e
pinsara-aasith <70972504+pinsara-aasith@users.noreply.github.com>
2020-09-11 21:50:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,55 +12,52 @@ var data = {
 var http = require('http');
 
 http.createServer(function (req, res) {
-  const fs = require('fs');
-  const carbone = require('carbone');
+  return res.end("My Name is aasith");
+ //  const fs = require('fs');
+ //  const carbone = require('carbone');
 
-  // Data to inject
-  // var data = {
-    // firstname : 'John',
-    // lastname : 'Doe'
-  // };
-	let data = []
-  req.on('data', chunk => {
-    data.push(chunk)
-  })
-  req.on('end', () => {
+ //  // Data to inject
+ //  // var data = {
+ //    // firstname : 'John',
+ //    // lastname : 'Doe'
+ //  // };
+	// let data = []
+ //  req.on('data', chunk => {
+ //    data.push(chunk)
+ //  })
+ //  req.on('end', () => {
 	  
-	res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-	"Access-Control-Allow-Origin":"*"});
+	// res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+	// "Access-Control-Allow-Origin":"*"});
 	
-	  try{
-    var dat = JSON.parse(data);
+	//   try{
+ //    var dat = JSON.parse(data);
 	
-    fs.writeFile('name.docx', result);
+ //    fs.writeFile('name.docx', result);
 	
-    fs.readFile('name.docx', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
-    res.write(data);
+ //    fs.readFile('name.docx', function(err, data) {
+ //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+ //    res.write(data);
 	
-    return res.end();
-	  }catch(e){
+ //    return res.end();
+	//   }catch(e){
 		  
 	  
-	  fs.readFile('name.docx', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
-    res.write(data);
-    return res.end();
-  });
-	  }
-	carbone.render('./test.docx', dat, function(err, result){
-    if (err) {
-      return console.log(err);
-    }
-    // write the result
+	//   fs.readFile('name.docx', function(err, data) {
+ //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+ //    res.write(data);
+ //    return res.end();
+ //  });
+	//   }
+	// carbone.render('./test.docx', dat, function(err, result){
+ //    if (err) {
+ //      return console.log(err);
+ //    }
+ //    // write the result
 	
     
-  });
+ //  });
 	
-  })
-  // Generate a report using the sample template provided by carbone module
-  // This LibreOffice template contains "Hello {d.firstname} {d.lastname} !"
-  // Of course, you can create your own templates!
-  
+ //  })
 }).listen(80);
 
