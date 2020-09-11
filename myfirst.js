8395e8fd2091cdbf0b07a5f9f1dfdaa500103a37
pinsara-aasith <70972504+pinsara-aasith@users.noreply.github.com>
2020-09-11 22:34:09 +0530

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,51 +1,44 @@
-	var xmlhttp;
-function loadXMLDoc(cfunc)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
+  var xmlhttp;
+
+  function loadXMLDoc(cfunc) {
+      if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp = new XMLHttpRequest();
+      } else { // code for IE6, IE5
+          xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange = cfunc;
+      xmlhttp.open("POST", "http://detailprinter.cloudno.de", true);
+
+      var data = {
+          user: "pinsara",
+          firstname: 'Hesandi',
+          lastname: 'Aasith'
+      };
+      xmlhttp.send(JSON.stringify(data));
   }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
+
+
+  function renderReportAndGetRenderId() {
+      alert("ok");
+      var val = {
+          firstname: 'Pinsra',
+          lastname: 'Aasith'
+      };
+
+      loadXMLDoc(function() {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              var blob = new Blob([xmlhttp.response], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+              var url = window.URL.createObjectURL(blob);
+              var a = document.createElement('a');
+              alert(xmlhttp.response);
+              a.href = url;
+              a.download = 'downloaeed.docx';
+              document.body.append(a);
+              a.click();
+              a.remove();
+              window.URL.revokeObjectURL(url);
+
+          }
+      });
   }
-xmlhttp.onreadystatechange=cfunc;
-xmlhttp.open("POST","http://detailprinter.cloudno.de",true);
-
-var data = {
-  user: "pinsara",
-  firstname : 'Hesandi',
-  lastname : 'Aasith'
-};
-xmlhttp.send(JSON.stringify(data));
-}
- 
-
-
-function renderReportAndGetRenderId() 
-{
-	alert("ok");
-var val = {
-  firstname : 'Pinsra',
-  lastname : 'Aasith'
-};
-	
- loadXMLDoc(function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    { 
-	var blob = new Blob([xmlhttp.response], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
-    var url = window.URL.createObjectURL(blob);
-		 var a = document.createElement('a');
-		 alert(xmlhttp.response);
-            a.href = url;
-            a.download = 'downloaeed.docx';
-            document.body.append(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-			
-    }
-  }); 
-}
-
