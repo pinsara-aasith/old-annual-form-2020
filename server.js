5dead1fae669a0a3cb48b106ec86c64ce72e25d1
pinsara-aasith <70972504+pinsara-aasith@users.noreply.github.com>
2020-09-11 22:08:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,38 +15,35 @@ http.createServer(function (req, res) {
   const fs = require('fs');
   const carbone = require('carbone');
 
-	let data = [];
-  req.on('data', chunk => {
-    data.push(chunk)
-  });
-  req.on('end', () => {
-    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    "Access-Control-Allow-Origin":"*"});
-      try{
-        var dat = JSON.parse(data);
-        carbone.render('./test.docx', dat, function(err, result){
-          fs.writeFile('name.docx', result);
-          fs.readFile('name.docx', function(err, data) {
-             res.write(data);
-          });
-        });
- //    if (err) {
- //      return console.log(err);
- //    }
- //    // write the result
+	// let data = [];
+ //  req.on('data', chunk => {
+ //    data.push(chunk)
+ //  });
+ //  req.on('end', () => {
+ //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+ //    "Access-Control-Allow-Origin":"*"});
+ //      try{
+ //        var dat = JSON.parse(data);
+ //        carbone.render('./test.docx', dat, function(err, result){
+ //          fs.writeFile('name.docx', result);
+ //          fs.readFile('name.docx', function(err, data) {
+ //             res.write(data);
+ //          });
+ //        });
+ // //    if (err) {
+ // //      return console.log(err);
+ // //    }
+ // //    // write the result
   
     
- //  });
+ // //  });
     
-    }catch(e){
-      return "Bad Request";
-    }
-
-
-
-  });
+ //    }catch(e){
+ //      return "Bad Request";
+ //    }
+ //  });
 	  
-	
+	 return res.end("ok i am bad");
 	//   try{
  //    var dat = JSON.parse(data);
 	
