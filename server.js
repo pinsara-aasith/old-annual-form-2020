7622951b1b80484581516471c19141ab8b5bac72
pinsara-aasith <70972504+pinsara-aasith@users.noreply.github.com>
2020-09-11 22:30:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,78 +11,79 @@
 // Of course, you can create your own templates!
 var http = require('http');
 
-http.createServer(function (req, res) {
-  const fs = require('fs');
-  const carbone = require('carbone');
+http.createServer(function(req, res) {
+    const fs = require('fs');
+    const carbone = require('carbone');
 
-	let data = [];
-  req.on('data', chunk => {
-    data.push(chunk)
-  });
-  req.on('end', () => {
-    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    "Access-Control-Allow-Origin":"*"});
-      try{
-        var dat = JSON.parse(data);
-        if(dat.user){
-
-        }else{
-
-        }
-        carbone.render('./test.docx', dat, function(err, result){
-          fs.writeFile('documents' + dat.user + '.docx', result);
-          fs.readFile('documents' + './' + dat.user +'.docx', function(err, data) {
-             res.write(data);
-             return res.end();
-          });
+    let data = [];
+    req.on('data', chunk => {
+        data.push(chunk)
+    });
+    req.on('end', () => {
+        res.writeHead(200, {
+            'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            "Access-Control-Allow-Origin": "*"
         });
- //    if (err) {
- //      return console.log(err);
- //    }
- //    // write the result
-  
-    
- //  });
-    
-    }catch(e){
-      fs.readFile('./test.docx', function(err, data) {
-             res.write(data);
-       return res.end();
-          });
-    }
-  });
+        try {
+            var dat = JSON.parse(data);
+            if (dat.user) {
+                carbone.render('./test.docx', dat, function(err, result) {
+                    fs.writeFile('documents/' + dat.user + '.docx', result);
+                    fs.readFile('documents' + '/' + dat.user + '.docx', function(err, data) {
+                        res.write(data);
+                        return res.end();
+                    });
+                });
+            } else {
+
+            }
+
+            //    if (err) {
+            //      return console.log(err);
+            //    }
+            //    // write the result
 
 
-	  
-	 // return res.end("ok i am bad");
-	//   try{
- //    var dat = JSON.parse(data);
-	
- //    fs.writeFile('name.docx', result);
-	
- //    fs.readFile('name.docx', function(err, data) {
- //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
- //    res.write(data);
-	
- //    return res.end();
-	//   }catch(e){
-		  
-	  
-	//   fs.readFile('name.docx', function(err, data) {
- //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
- //    res.write(data);
- //    return res.end();
- //  });
-	//   }
-	// carbone.render('./test.docx', dat, function(err, result){
- //    if (err) {
- //      return console.log(err);
- //    }
- //    // write the result
-	
-    
- //  });
-	
- //  })
+            //  });
+
+        } catch (e) {
+            fs.readFile('./test.docx', function(err, data) {
+                res.write(data);
+                return res.end();
+            });
+        }
+    });
+
+
+
+    // return res.end("ok i am bad");
+    //   try{
+    //    var dat = JSON.parse(data);
+
+    //    fs.writeFile('name.docx', result);
+
+    //    fs.readFile('name.docx', function(err, data) {
+    //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+    //    res.write(data);
+
+    //    return res.end();
+    //   }catch(e){
+
+
+    //   fs.readFile('name.docx', function(err, data) {
+    //    res.writeHead(200, {'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+    //    res.write(data);
+    //    return res.end();
+    //  });
+    //   }
+    // carbone.render('./test.docx', dat, function(err, result){
+    //    if (err) {
+    //      return console.log(err);
+    //    }
+    //    // write the result
+
+
+    //  });
+
+    //  })
 }).listen(80);
-
